282f49f5856cad08ba2288619c0b04165eb2764d
Ranga Sashank Gurram <rangasashakgurram@uvic.ca>
2024-11-23 16:43:22 -0800

    async in mongo

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
-export const connectDB = () => {
+export const connectDB = async () => {
   try {
-      mongoose.connect(process.env.MONGO_URI, {dbName: "flatmatebackend",}).then((c) => console.log(`Database Connected with ${c.connection.host}`)).catch((e) => console.log(e));
+      await mongoose.connect(process.env.MONGO_URI, {dbName: "flatmatebackend",}).then((c) => console.log(`Database Connected with ${c.connection.host}`)).catch((e) => console.log(e));
 
   } catch (error) {
     console.error(`Error: ${error.message}`);
