fd2c5061d21a6af113f1bebf7ec5ffeb05f2140c
Ranga Sashank Gurram <rangasashakgurram@uvic.ca>
2024-11-26 00:50:03 -0800

    found bug

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -38,7 +38,7 @@ export const createGroup = async(req, res) => {
 export const getMembers = async(req, res) => {
   try {
     const group = await RoommateGroup.findById(req.params.groupId)
-    res.status(200).json(group);
+    res.status(200).json(group.members);
   }
   catch {
     res.status(500).json({error: error.message})
