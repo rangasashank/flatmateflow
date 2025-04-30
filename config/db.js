cb65fe890a6392590adf7e4386486247c1ea8895
Ranga Sashank Gurram <rangasashakgurram@uvic.ca>
2025-04-30 02:11:39 -0700

    done removing config

---
+++ config/db.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+  try {
+      await mongoose.connect(process.env.MONGO_URI, {dbName: "flatmatebackend",}).then((c) => console.log(`Database Connected with ${c.connection.host}`)).catch((e) => console.log(e));
+
+      
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
